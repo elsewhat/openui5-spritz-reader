bcdfcba90a56359661a54186d7da13c863cb7242
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2014-03-03 20:31:02 +0100

    Added items support and events

--- openui5-spritz-reader.js
+++ openui5-spritz-reader.js
@@ -19,14 +19,26 @@ sap.ui.core.Control.extend("open.m.SpritzReader", {
     },
 
     start:function(){
+        //TODO: Reset if already reading
         var that=this;
         this.fireReadingStarting();
         //TODO: Check if we are using text or multiple items
-
+        var hasItems=false;
+        var text;
+        var readerItems;
+        var readerItemIndex=0;
+        //if items aggregation exist use it, if not use text property
+        if(that.getItems() && that.getItems().length>0){       
+            readerItems = that.getItems();
+            console.log("openui5-spritz-reader. Reading " + readerItems.length + " items");
+            text= readerItems[readerItemIndex].getText();
+            hasItems=true;
+        }else {
+            text = that.getText(); 
+        }
 
         //clearTimeout(currentTimer);
         var currentWord = 0;
-        var text = that.getText();
         var words = text.split(/\s+/).map(that._processWord);
 
         var displayNextWord = function(){
@@ -34,22 +46,38 @@ sap.ui.core.Control.extend("open.m.SpritzReader", {
             var readerEl = $(".spritzreader");
             var currentTimer = null;
 
-
             var wpm = that.getWordsPrMin();
             var delay = 60000 / parseInt(wpm, 10);
-            //end refactor
 
-
-            var word = words[currentWord++];
-            // WTB> nlp.js...
+            var word = words[currentWord];
             var hasPause = /^\(|[,\.\)]$/.test(word);
             
             // XSS?! :(.
+            //TODO: Escape for XSS
             readerEl.children().first().html(word);
             that._positionWord();
             
-            if (currentWord !== words.length){
+            //check if we should read more words
+            if (currentWord < words.length-1){
+                currentWord++
                 currentTimer = setTimeout(displayNextWord, delay * (hasPause ? 2 : 1));
+            }else if(hasItems){
+                if(readerItemIndex < readerItems.length-1){
+                    that.fireReadingItemComplete({item:readerItems[readerItemIndex]});
+                    currentWord=0;
+                    readerItemIndex++;
+                    text= readerItems[readerItemIndex].getText();
+                    words = text.split(/\s+/).map(that._processWord);
+                    currentTimer = setTimeout(displayNextWord, delay * 3);
+                    
+                    //should we trigger this after the timeout is complete?
+                    that.fireReadingItemStarting({item:readerItems[readerItemIndex]}); 
+                }else {
+                    that.fireReadingItemComplete({item:readerItems[readerItemIndex]});
+                    that.fireReadingComplete(); 
+                }
+            }else {
+                that.fireReadingComplete(); 
             }
         };
         
